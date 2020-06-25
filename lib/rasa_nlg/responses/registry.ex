e93880ea6c34444902e018561b91c24a38b28e47
Dirk Elmendorf <dirk@r26d.com>
2020-06-24 19:40:43 -0500

    Added in support for namespacing the responses. This allows you to serve multiple bots from a single server

--- lib/rasa_nlg/responses/registry.ex
+++ lib/rasa_nlg/responses/registry.ex
@@ -1,27 +1,37 @@
 defmodule RasaNLG.Responses.Registry do
   alias RasaNLG.Responses.Context
 
-  def register_responses(modules) do
+  def register_responses( modules) when is_list(modules) do
+    register_responses({nil, modules})
+  end
+  def register_responses({prefix, modules}) do
     responses_table = get_responses_table()
     :ets.new(responses_table, [:set, :protected, :named_table])
 
     modules
     |> Enum.each(fn module ->
       if RasaNLG.Responses.Response in (module.module_info(:attributes)[:behaviour] || []) do
-        IO.puts("Registering #{module.name}")
-        :ets.insert(responses_table, {module.name(), module})
+       # IO.puts("Registering #{key_name(prefix, module.name())}")
+        :ets.insert(responses_table, {key_name(prefix, module.name()), module})
       end
     end)
   end
 
-  def execute(%Context{request: %{template: response_name}} = context) do
-    case :ets.lookup(get_responses_table(), response_name) do
+  defp key_name(nil, name), do: name
+  defp key_name(prefix, name) do
+    "#{prefix}/#{name}"
+  end
+  def execute(%Context{} = context), do: execute(context, [prefix: nil])
+  def execute(%Context{request: %{template: response_name}} = context, [prefix: prefix]) do
+ #   IO.puts("Going to look for #{key_name(prefix, response_name)}")
+    case :ets.lookup(get_responses_table(), key_name(prefix, response_name)) do
       [] ->
+       # IO.puts("Could not find it")
         context
         |> Context.set_error(response_name, "response not found")
 
       [{_, module}] ->
-       IO.puts("Responding from #{response_name}")
+    #   IO.puts("Responding from #{key_name(prefix, response_name)}")
         module.run(context)
     end
   end
