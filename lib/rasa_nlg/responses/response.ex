7108df74454a6affe1cbf9d1e895f006c610ca04
Dirk Elmendorf <dirk@r26d.com>
2020-06-25 18:28:09 -0500

    feat: Added in a simple_text method to dramatically reduce how much code is requried for an utterance. feat: added in support for a default personality to be able to customize responses.

--- lib/rasa_nlg/responses/response.ex
+++ lib/rasa_nlg/responses/response.ex
@@ -1,5 +1,6 @@
 defmodule RasaNLG.Responses.Response do
   alias RasaNLG.Responses.Context
+  alias RasaNLG.Model.Response, as: ResponseModel
 
   @callback name() :: String.t()
   @callback run(Context.t()) :: Context.t()
@@ -13,14 +14,33 @@ defmodule RasaNLG.Responses.Response do
         "#{__MODULE__}"|>String.split(".")|>Enum.reverse|>Enum.at(0)|>Macro.underscore()
       end
       def preprocess(%Context{} = context), do: context
-      def respond(%Context{} = context), do: context
+      @doc """
+          This is an overrideable method to make a response.
+      """
+      def respond(%Context{} = context) do
+       case simple_text() do
+         nil -> context
+         value ->
+           context
+           |> set_response(%ResponseModel{text: value}, personality)
+       end
+
+      end
+
+      def personality(), do: nil
       def run(%Context{} = context) do
         context
         |> preprocess()
         |> respond()
       end
+      @doc """
+        This is a text response. You can override this method to make a simple response.
+      """
+      def simple_text(), do: nil
       defoverridable preprocess: 1,
+                     simple_text: 0,
                      name: 0,
+                     personality: 0,
                      respond: 1
     end
   end
